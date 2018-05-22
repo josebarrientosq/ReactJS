8fafc63a22b7f5122af96c2e0ba441df4569737e
josebarrientosq <35469345+josebarrientosq@users.noreply.github.com>
2018-05-21 22:51:06 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+//import titulo from './titulo.jsx';
+//import buscador from './buscador.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+//ReactDOM.render(<titulo />, document.getElementById('tuto'));
+ReactDOM.render(<buscador />, document.getElementById('buscador'));
