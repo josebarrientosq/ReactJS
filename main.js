17d8f88b6d1db0579186ebfd2efefe69e97d0273
josebarrientosq <35469345+josebarrientosq@users.noreply.github.com>
2018-05-21 23:06:27 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-//import titulo from './titulo.jsx';
-//import buscador from './buscador.jsx';
+import buscador from './buscador.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
 ReactDOM.render(<buscador />, document.getElementById('buscador'));
