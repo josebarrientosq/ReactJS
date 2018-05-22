ee17539984849daa995d9c2e7410ae01e11d504a
josebarrientosq <35469345+josebarrientosq@users.noreply.github.com>
2018-05-21 22:49:53 -0500

    Create buscador.jsx

---
+++ buscador.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class buscador extends React.Component{
+    render(){
+        return(
+            <div>
+                Hola buscador
+              </div>
+            );
+    }
+}
+
+module.exports buscador;
