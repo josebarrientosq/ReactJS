2636fb57e055a47852d2c46790c55688424a2e4b
josebarrientosq <35469345+josebarrientosq@users.noreply.github.com>
2018-05-31 23:28:51 -0500

    Update buscador.jsx

--- buscador.jsx
+++ buscador.jsx
@@ -3,9 +3,15 @@ import React from 'react';
 class buscador extends React.Component{
     render(){
         return(
-            <div>
-                Hola buscador
-              </div>
+        	 <div class="row">
+					<div class="col s4">
+						<h4> Catálogo de productos </h4>
+					</div>
+					<div class="input-field col s3 offset-s5">
+						<label for="first_name">¿Qué estas buscando?</label>
+				        <input placeholder="Buscar producto" id="first_name" type="text" class="validate"/>
+ 		    		</div>
+				</div>
             );
     }
 }
