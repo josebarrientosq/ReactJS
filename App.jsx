aa25810d875284ab0b5a5efaa2a51b8c7fdd4168
josebarrientosq <35469345+josebarrientosq@users.noreply.github.com>
2018-05-31 23:28:10 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,14 +1,59 @@
 import React from 'react';
+import data from './data.json';
+
 
 class App extends React.Component{
+	
+	verimagen(imageUrl) {
+    return (
+      <div>
+        <img src={imageUrl} />
+      </div>
+    );
+  }
+
     render(){
         return(
-            <div>
-                Hola React
-              </div>
-            );
+            
+        
+          data.map(function(movie){
+
+         
+
+            return ( <div class="col s4">
+				  		<div class="row">
+					        <div class="col s12 ">
+					          	<div class="card">
+						            <div class="card-image">
+						            <img src={require('./imagesBase/aguacate.jpg')}
+						            </div>
+						            <div class="card-content">
+						              	<h5> {movie.title}</h5>
+									  	<p> {movie.id} </p>
+									    <p> Unidades disponibles : 46 </p>
+									    <div class="row">
+									    	<div class="col s5">
+									    		<a class="waves-effect waves-light btn-small light-blue darken-3">Ver Mas</a>
+									    	</div>
+									    	<div class="col s4 ">
+									    		<a class="waves-effect waves-light btn-small amber darken-1">Añadir</a>
+									    	</div>
+									    	<div class="col s3">
+									    		<input class="cantidad" type="number" min="1" max="9" step="1" value="1"/>
+									    	</div>
+									    </div>
+						            </div>
+						        </div>
+		        			</div>
+		        		</div> 
+		        	</div>);  
+		        	
+          })
+        
+        
+        );
     }
 }
 
 
-export default App;
+module.exports = App;
