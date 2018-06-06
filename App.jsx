0dd26e60001e2709bd667eb8e8a00ef0c8205551
josebarrientosq <35469345+josebarrientosq@users.noreply.github.com>
2018-06-05 20:53:29 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import data from './data.json';
 import Producto from './Producto.jsx'
-
+import Modal from './Modal.jsx'
 
 
 class App extends React.Component{
-	
-	
+
+	 	
     render(){
         return(
-            
+             
         
           data.map(function(productos){
 
@@ -24,15 +24,24 @@ class App extends React.Component{
 						            	
 						            </div>
 						            <div class="card-content">
-						              	<h5> {productos.title}</h5>
-									  	<p> {productos.id} </p>
-									    <p> Unidades disponibles : 46 </p>
+						              	<h5>{productos.title}</h5>
+									  	<p> Precio : S/.{productos.precio} </p>
+									    <p> Unidades disponibles : {productos.stock} </p>
 									    <div class="row">
 									    	<div class="col s5">
-									    		<a class="waves-effect waves-light btn-small light-blue darken-3">Ver Mas</a>
+
+									    		<a class="waves-effect waves-light btn modal-trigger" href={'#'+productos.title}>Ver Mas</a>
+
+									    		<Modal idmodal={productos.title} precio={productos.precio}/>
+
+									    		
 									    	</div>
+
+									    	
+
+
 									    	<div class="col s4 ">
-									    		<a class="waves-effect waves-light btn-small amber darken-1">Añadir</a>
+									    		<button class="agregar waves-effect waves-light btn-small amber darken-1"  >Añadir</button>
 									    	</div>
 									    	<div class="col s3">
 									    		<input class="cantidad" type="number" min="1" max="9" step="1" value="1"/>
